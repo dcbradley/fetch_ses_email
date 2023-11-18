db26cff8793ae205a6e15fe3aad12dbb97ec8ee1
Dan Bradley <WasteEnemy@gmail.com>
2023-11-18 14:44:01 +0000

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fetch_ses_email
 Fetch email from an EC2 S3 bucket fed by Amazon SES.
+
+Based on a code snippet from https://markw.dev/aws-free-email/
+This variant drops the email in an inbox in maildir format,
+and it adds support for optionally running as root and
+writing into some other user's mailbox.
---
+++ fetch_ses_email.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import boto3
+import mailbox
+import argparse
+import pathlib
+import os
+import pwd
+import sys
+
+parser = argparse.ArgumentParser(description='Fetch email from an EC2 S3 bucket fed by Amazon SES.')
+parser.add_argument('bucket_name')
+parser.add_argument('user',default=os.getlogin())
+args = parser.parse_args()
+
+s3_bucket = args.bucket_name
+
+# Create s3 client
+client = boto3.client('s3')
+
+# Get new mail
+response = client.list_objects_v2(
+    Bucket=s3_bucket,
+    Delimiter=','
+)
+
+if not ('Contents' in response):
+    sys.exit(0)
+
+new_messages = [msg['Key'] for msg in response['Contents'] if not msg['Key'].startswith('processed/')]
+
+if args.user != os.getlogin():
+    p = pwd.getpwnam(args.user)
+    if not p:
+        sys.stderr.write("Failed to find user with name " + args.user)
+        sys.exit(1)
+    os.setuid(p.pw_uid)
+    homedir = p.pw_dir
+else:
+    homedir = str(pathlib.Path.home())
+
+maildir = homedir + "/Maildir"
+
+if new_messages:
+    inbox = mailbox.Maildir(maildir)
+
+    s3 = boto3.resource('s3')
+    for i in new_messages:
+        try:
+            message_content = s3.Object(s3_bucket, i).get()['Body'].read().decode()
+        except UnicodeDecodeError:
+            message_content = s3.Object(s3_bucket, i).get()['Body'].read().decode('cp1252')
+
+        inbox.add(message_content)
+
+        #s3.Object(s3_bucket,f"processed/{i}").copy_from(CopySource=f"{s3_bucket}/{i}")
+        s3.Object(s3_bucket,i).delete()
